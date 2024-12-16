03042d53bfeda2675db80b5de68ecc4f623e1800
Dylan Savelson <123187510+DylanSavelson@users.noreply.github.com>
2024-12-15 20:30:59 -0500

    iron drops from stone

--- src/objects/Iron.js
+++ src/objects/Iron.js
@@ -3,31 +3,39 @@ import GameObject from './GameObject.js';
 import Vector from '../../lib/Vector.js';
 import { getCollisionDirection } from '../../lib/Collision.js';
 import Stone from './Stone.js';
+import { timer } from '../globals.js';
+import Easing from '../../lib/Easing.js';
+import Hitbox from '../../lib/Hitbox.js';
 
 export default class Iron extends Stone {
-    static WIDTH = 32;
-    static HEIGHT = 32;
-    static dimensions = new Vector(this.WIDTH,this.HEIGHT)
     /**
      * The first tier ore strong enough to help cultivate more rare ores worth more.
      */
     constructor(sprites, position, player) {
         super(sprites, position, player); 
-        
     }
 
     
     update(dt)
     {
         super.update(dt);
-        if (this.mined && this.currentFrame === 11)
-        {
-            this.sprites = this.oreSprites
-            this.currentFrame = 0;
-        }
+        this.setPositionsAfterMined();
     }
 
-
-
+    setPositionsAfterMined()
+    {
+        if (this.mined && this.currentFrame === 11)
+            {
+                this.sprites = this.oreSprites
+                this.currentFrame = 0;
+                this.dimensions = new Vector(Stone.OREHEIGHT, Stone.OREWIDTH);
+                this.hitbox = new Hitbox(
+                    this.position.x + this.hitboxOffsets.position.x,
+                    this.position.y + this.hitboxOffsets.position.y,
+                    this.dimensions.x + this.hitboxOffsets.dimensions.x,
+                    this.dimensions.y + this.hitboxOffsets.dimensions.y,
+                );
+            }
+    }
 
 }
--- src/objects/Stone.js
+++ src/objects/Stone.js
@@ -3,8 +3,9 @@ import GameObject from './GameObject.js';
 import Vector from '../../lib/Vector.js';
 import { getCollisionDirection } from '../../lib/Collision.js';
 import Sprite from '../../lib/Sprite.js';
-import { images } from '../globals.js';
+import { images, timer } from '../globals.js';
 import ImageName from '../enums/ImageName.js';
+import Easing from '../../lib/Easing.js';
 
 export default class Stone extends GameObject {
 	static WIDTH = 32;
@@ -30,6 +31,9 @@ export default class Stone extends GameObject {
 			Stone.OREWIDTH,
 			Stone.OREHEIGHT
 		);
+		this.hovering = false;
+		this.positionChanged = true;
+
 	}
 
 	
@@ -52,11 +56,25 @@ export default class Stone extends GameObject {
 				this.isSolid = false;
 			}
 		}
-		if (this.mined && this.currentFrame < 11)
+		if (this.mined)
+		{
+			if (this.positionChanged)
+			{
+				this.position.x += 8;
+				this.position.y += 11;
+				this.positionChanged = false;
+			}
+			if(!this.hovering)
+			{
+				this.hovering = true;
+				this.hoverOre();
+			}
+			if(this.currentFrame < 11)
 			{
 				this.currentAnimation?.update(dt);
 				this.currentFrame = this.explodeAnimation.getCurrentFrame();
 			}
+		}
     }
     
 	onCollision(collider) {
@@ -64,6 +82,24 @@ export default class Stone extends GameObject {
 		this.checkTargetedStone(collider);
 		
 	}
+	
+    async hoverOre()
+    {
+        const movement = 5;
+        await timer.tweenAsync(
+			this.position,
+			{  y: this.position.y + movement },
+			1.2,
+            Easing.easeInOutQuad
+		);
+        await timer.tweenAsync(
+			this.position,
+			{  y: this.position.y - movement },
+			1.2,
+            Easing.easeInOutQuad
+		);
+        this.hovering = false;
+    }
 
 	checkTargetedStone(collider) 
 	{
